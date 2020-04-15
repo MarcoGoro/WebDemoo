8b5f3aea1438a252b865d9bfe36e426dec2939bf
Marco Gorelli <marco.gorelli23@gmail.com>
2020-04-15 16:57:45 +0200

    modifica-Startup-e-aggiunta-pacchetti-NuGet

--- WebDemoo/Startup.cs
+++ WebDemoo/Startup.cs
@@ -10,8 +10,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
-namespace WebDemoo
+namespace WebDemo
 {
     public class Startup
     {
@@ -26,17 +27,22 @@ namespace WebDemoo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseSwagger();
 
-            app.UseHttpsRedirection();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseRouting();
 
